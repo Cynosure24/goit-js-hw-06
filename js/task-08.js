b8becbcacd3f7e0eef8fccfe33baccaef48d9a89
Cynosure24 <139391583+Cynosure24@users.noreply.github.com>
2023-11-14 20:55:46 +0100

    update

--- js/task-08.js
+++ js/task-08.js
@@ -3,17 +3,17 @@ form.addEventListener(`submit`, handleSubmit);
 
 function handleSubmit(event) {
     event.preventDefault();
-    const { elements: { email, password } } = event.currentTarget;
-    //const password: any
+
+    const {
+      elements: { email, password },
+    } = event.currentTarget;
     if (email.value === "" || password.value === "") {
         alert(`Всі поля повинні бути заповнені`)
+      } else {
+        console.log({
+          email: email.value,
+          password: password.value,
+        });
+      }
+      event.currentTarget.reset();
     }
-    console.log("email: "
-        `${email.value}`, "password: "
-        `${password.value}`);
-    event.currentTarget.reset();
-}
-
-function multiply(...form) {
-    console.log(form)
-}
